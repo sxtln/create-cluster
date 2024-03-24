a14f28ed91fdee041d63c984e2e5f1a812280ad5
Sagi Forbes <sagi.forbes@myhome.loc>
2024-03-24 23:26:56 +0200

    send the name of the new cluster

--- provision-cluster.py
+++ provision-cluster.py
@@ -11,8 +11,9 @@ parser.add_argument('--node-count', type=int, default=1,
                     help='number of nodes in cluster. can be 1,3,4,5....')
 parser.add_argument('--node-type', choices=['b1', 'b2'],
                     default='medium', help='size of a node in the cluster')
-parser.add_argument('--apikey',required=True)
-parser.add_argument('--cluster-name',required=False,help="a user denote name of the cluster")
+parser.add_argument('--apikey', required=True)
+parser.add_argument('--cluster-name', required=False,
+                    help="a user denote name of the cluster")
 parser.add_argument('--output', default='k8s.config',
                     help='name of file to save the configuration to')
 
@@ -30,8 +31,9 @@ node_type = args.node_type
 req_headers = {'Authorization': f'Bearer {args.apikey}'}
 try:
     conn = http.client.HTTPSConnection('api.sextillion.io')
-    req_data = {'nodeCount': node_count, 'nodeType': node_type}
-    
+    req_data = {'nodeCount': node_count,
+                'nodeType': node_type, 'name': args.cluster_name}
+
     conn.request('POST', '/sc/cluster', json.dumps(req_data),
                  headers=req_headers)
     hres = conn.getresponse()
@@ -44,11 +46,11 @@ try:
 
     clusterId = hres_data["clusterId"]
     print(f'provisioning cluster {clusterId}')
-    output_file=os.environ.get('GITHUB_OUTPUT') if os.environ.get('GITHUB_OUTPUT') else 'clusterId.txt'
-    with open(output_file,'at') as file:
-      file.write(f'clusterId={clusterId}\n')
-    
-    
+    output_file = os.environ.get('GITHUB_OUTPUT') if os.environ.get(
+        'GITHUB_OUTPUT') else 'clusterId.txt'
+    with open(output_file, 'at') as file:
+        file.write(f'clusterId={clusterId}\n')
+
     clusterIp = ''
     while hres_data['stage'] != 'k8sReady' or hres_data['stageIndex'] < 9:
         time.sleep(3)
@@ -59,7 +61,7 @@ try:
             hres_data = json.loads(res_body)
 
             if hres_data['nodes']:
-                clusterIp=hres_data['nodes'][0]['ip']
+                clusterIp = hres_data['nodes'][0]['ip']
 
         elif (hres.status == 404):
             print(f'cluster {clusterId} not found')
@@ -68,7 +70,8 @@ try:
             hres_data['stage'] = 'unknown'
             print(f'failed to fetch cluster info {hres.status}')
 
-        print(f'cluster {clusterId} stage {int(round((hres_data["stageIndex"]/9)*100,0))}%')
+        print(
+            f'cluster {clusterId} stage {int(round((hres_data["stageIndex"]/9)*100,0))}%')
 
     if hres_data['stage'] == 'k8sReady':
         conn.request(
@@ -77,18 +80,19 @@ try:
         if (hres.status == 200):
             fileContent = hres.read()
             with open(args.output, 'wb') as file:
-              file.write(fileContent)
-            
-            output_file=os.environ.get('GITHUB_OUTPUT') if os.environ.get('GITHUB_OUTPUT') else 'clusterId.txt'
-            with open(output_file,'at') as file:
-              file.write(f'clusterIp={clusterIp}\n')
+                file.write(fileContent)
+
+            output_file = os.environ.get('GITHUB_OUTPUT') if os.environ.get(
+                'GITHUB_OUTPUT') else 'clusterId.txt'
+            with open(output_file, 'at') as file:
+                file.write(f'clusterIp={clusterIp}\n')
         else:
             print('failed to fetch configuration', hres.status)
             exit(1)
     else:
         print('failed to start cluster last known status', hres_data['stage'])
         exit(1)
-    
+
 except Exception as ex:
     print('failed to deploy cluster', ex)
     exit(1)
